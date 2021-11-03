ba28528afc6097e1e33fc398f4b5ad49448be232
Lucas Kennedy <lukakfp@gmail.com>
2021-11-03 19:07:39 -0300

    refatoração

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "market_cubos",
-  password: "123456",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 const query = (text, param) => {
